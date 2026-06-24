8f62a1b4d50d8d679422fe4f8749b85232c84524
Laurent Marquet <laurent.marquet@laposte.net>
2026-06-24 14:17:16 +0200

    - Creation of bundle (24/06/2026)

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+# ChangeLog
+
+## v0.1
+
+- Creation of bundle (24/06/2026)
---
+++ src/c975LUiBundle.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * (c) 2018: 975L <contact@975l.com>
+ * (c) 2018: Laurent Marquet <laurent.marquet@laposte.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace c975L\UiBundle;
+
+use c975L\UiBundle\DependencyInjection\Compiler\MenuProviderPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class c975LUiBundle extends AbstractBundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new MenuProviderPass());
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        $containerConfigurator->import('../config/services.yaml');
+    }
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
